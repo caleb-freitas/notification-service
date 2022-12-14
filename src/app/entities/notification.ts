625fb0dcceecc5c850a69a9c075d735024dbce0d
caleb-freitas <calebfreitas@tutanota.com>
2022-12-13 21:10:00 -0300

    feat: added notification entity

---
+++ src/app/entities/notification.ts
@@ -0,0 +1,53 @@
+import { Content } from './content';
+
+export interface NotificationProps {
+  recipientId: string;
+  content: Content;
+  category: string;
+  readedAt?: Date | undefined;
+  createdAt: Date;
+}
+
+export class Notification {
+  private props: NotificationProps;
+
+  constructor(props: NotificationProps) {
+    this.props = props;
+  }
+
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
+
+  public get recipientId(): string {
+    return this.props.recipientId;
+  }
+
+  public set content(content: Content) {
+    this.props.content = content;
+  }
+
+  public get content(): Content {
+    return this.props.content;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set readedAt(readedAt: Date | null | undefined) {
+    this.props.readedAt = readedAt;
+  }
+
+  public get readedAt(): Date | null | undefined {
+    return this.props.readedAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
+}
